a33ea5ee43b24966cc454da341c932c6387d313b
nikita-vladimirov <vladimirov46@gmail.com>
2019-06-28 16:53:21 +0300

    Реализовывает настройки мага

--- js/setup.js
+++ js/setup.js
@@ -1,8 +1,5 @@
 'use strict';
 
-var setup = document.querySelector('.setup');
-setup.classList.remove('hidden');
-
 function getRandomValue(min, max) {
   var randomValue = min + Math.random() * (max + 1 - min);
   randomValue = Math.floor(randomValue);
@@ -31,3 +28,56 @@ fragment.appendChild(similarWizardTemplate);
 document.querySelector('.setup-similar-list').appendChild(fragment);
 
 document.querySelector('.setup-similar').classList.remove('hidden');
+
+var setupWindow = document.querySelector('.setup');
+var buttonOpenSetup = document.querySelector('.setup-open');
+var buttonCloseSetup = document.querySelector('.setup-close');
+var buttonSubmit = document.querySelector('.setup-submit');
+var inputWizzardName = document.querySelector('.setup-user-name');
+
+var toggleSetupWindow = function (button) {
+  button.addEventListener('click', function () {
+    setupWindow.classList.toggle('hidden');
+  });
+};
+
+document.addEventListener('keydown', function (evt) {
+  console.log(document.activeElement.className);
+  if (document.activeElement.className === 'setup-open-icon' && evt.keyCode === 13) {
+    setupWindow.classList.remove('hidden');
+  } else if (evt.keyCode === 27 && document.activeElement.className !== 'setup-user-name') {
+    setupWindow.classList.add('hidden');
+  } else if (evt.keyCode === 13 && document.activeElement.className === 'setup-close') {
+    setupWindow.classList.add('hidden');
+  } else if (evt.keyCode === 13) {
+    buttonSubmit.click();
+  }
+});
+
+toggleSetupWindow(buttonOpenSetup);
+toggleSetupWindow(buttonCloseSetup);
+
+var wizardCoat = document.querySelector('.setup-wizard').querySelector('.wizard-coat');
+var wizardEyes = document.querySelector('.setup-wizard').querySelector('.wizard-eyes');
+var fireBall = document.querySelector('.setup-fireball-wrap');
+
+var fireBallColorList = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+wizardCoat.addEventListener('click', function () {
+  var wizardCoatColor = coatColorList[getRandomValue(0, coatColorList.length - 1)];
+  wizardCoat.style.fill = wizardCoatColor;
+  var wizardCoatInput = document.querySelector('input[name="coat-color"]');
+  wizardCoatInput.value = wizardCoatColor;
+});
+wizardEyes.addEventListener('click', function () {
+  var wizardEyesColor = eyesColorList[getRandomValue(0, eyesColorList.length - 1)];
+  wizardEyes.style.fill = wizardEyesColor;
+  var wizardEyesInput = document.querySelector('input[name="eyes-color"]');
+  wizardEyesInput.value = wizardEyesColor;
+});
+
+fireBall.addEventListener('click', function () {
+  var fireBallColor = fireBallColorList[getRandomValue(0, fireBallColorList.length - 1)];
+  fireBall.style.backgroundColor = fireBallColor;
+  fireBall.querySelector('input').value = fireBallColor;
+});
